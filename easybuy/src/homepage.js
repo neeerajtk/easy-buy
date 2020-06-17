370314cc2abfb2cb975bfc30b43b85865c47a4bf
neeerajtk <neerajtk560@gmail.com>
2020-06-17 17:52:36 +0530

    home comp

---
+++ easybuy/src/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () =>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+
+            </div>
+        </div>
+    </div>
+)
+
