547f7a1336c33da9b0f85a91cb51883033ca95da
neeerajtk <neerajtk560@gmail.com>
2020-06-17 18:14:43 +0530

    minor tweaks

--- easybuy/src/homepage.js
+++ easybuy/src/homepage.js
@@ -21,12 +21,6 @@ const HomePage = () =>(
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>            
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
            <div className='menu-item'>
                 <div className='content'>
                     <h1 className="title">MENS</h1>
