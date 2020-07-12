aa932f6f547342f279b88b6ced40e71a6a18eb71
neeerajtk <neerajtk560@gmail.com>
2020-07-12 23:53:15 +0530

    styling changes

--- easybuy/src/components/menu-item/menu-item.js
+++ easybuy/src/components/menu-item/menu-item.js
@@ -3,12 +3,15 @@ import './menu-item.scss';
 
 const MenuItem = ({title,imageUrl,size}) => (
 
+    <div className={`${size} menu-item`}>
     <div 
+    className="background-image"  
     style={{
         backgroundImage: `url(${imageUrl})`
-        }}
-        className={`${size} menu-item`}
-        >
+        }} >
+
+            
+        </div>
         <div className='content'>           
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
