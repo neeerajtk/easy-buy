7ac94da8939bd011f38c97a0543817dc83beeaa5
neeerajtk <neerajtk560@gmail.com>
2020-07-12 23:29:24 +0530

    images added

--- easybuy/src/components/menu-item/menu-item.js
+++ easybuy/src/components/menu-item/menu-item.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './menu-item.scss';
 
-const MenuItem = ({title}) => (
+const MenuItem = ({title,imageUrl}) => (
 
-    <div className='menu-item'>
+    <div className='menu-item' style={{backgroundImage: `url(${imageUrl})`}}>
         <div className='content'>           
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
