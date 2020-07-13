304f4dc15a278ca65f4ac940650a5be4ccaec2c4
neeerajtk <neerajtk560@gmail.com>
2020-07-13 10:53:53 +0530

    sections added

--- easybuy/src/components/directory/directory.js
+++ easybuy/src/components/directory/directory.js
@@ -10,33 +10,33 @@ class Directory extends React.Component {
         this.state = {
             sections : [
                 {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  title: 'Home & Living',
+                  imageUrl: 'https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
                   id: 1,
                   linkUrl: 'shop/hats'
                 },
                 {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  title: 'Electronics',
+                  imageUrl: 'https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
                   id: 2,
                   linkUrl: 'shop/jackets'
                 },
                 {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  title: 'Automotive',
+                  imageUrl: 'https://images.unsplash.com/photo-1539799139339-50c5fe1e2b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80.png',
                   id: 3,
                   linkUrl: 'shop/sneakers'
                 },
                 {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  title: 'womens Wear',
+                  imageUrl: 'https://images.unsplash.com/photo-1574721569234-c85f77aaefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=963&q=80',
                   size: 'large',
                   id: 4,
                   linkUrl: 'shop/womens',
                 },
                 {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  title: 'mens wear',
+                  imageUrl: 'https://images.unsplash.com/photo-1543960713-7538001f7c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
                   size: 'large',
                   id: 5,
                   linkUrl: 'shop/mens'
