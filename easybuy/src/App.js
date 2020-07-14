f57401dd34a2eda3d16bf758d5ec43fcb8a565e4
neeerajtk <neerajtk560@gmail.com>
2020-07-14 23:44:44 +0530

    route init

--- easybuy/src/App.js
+++ easybuy/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage'
 
+const HomesPage = () => (
+  <div>
+    <h1>HOME / LIVING</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Route exact path='/' component={HomePage} />
     </div>
   );
 }
