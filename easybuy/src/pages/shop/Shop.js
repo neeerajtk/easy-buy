9d5f36273fbe1ca8d9c68b9e89c738a8e2e132a2
neeerajtk <neerajtk560@gmail.com>
2020-07-24 21:23:33 +0530

    home shop component added

---
+++ easybuy/src/pages/shop/Shop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShopPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
