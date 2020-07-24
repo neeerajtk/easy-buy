939eb8989982beff685680401885a3cc9ca8746c
neeerajtk <neerajtk560@gmail.com>
2020-07-24 23:18:38 +0530

    preview add

---
+++ easybuy/src/components/collection-preview/collection-preview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './collection-preview.scss';
+
+
+const CollectionPreview = () => (
+    <div className="collection-preview">
+        <h1>Title</h1>
+        <div className='preview'>
+            
+        </div>
+    </div>
+)
