79dcff6f33131792875807decdfb08c552182889
neeerajtk <neerajtk560@gmail.com>
2020-07-26 23:17:48 +0530

    user fetch // firebase

--- easybuy/src/App.js
+++ easybuy/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
@@ -7,19 +7,37 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import Header from './components/header/header';
+import {auth} from './firebase/firebase';
 
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser : null
+    };
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+      console.log(user);
+      
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
   );
+  }
 }
 
 export default App;
