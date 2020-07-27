e02af677a97feb90ab34e9e1df1b81490c424a53
neeerajtk <neerajtk560@gmail.com>
2020-07-27 21:17:28 +0530

    login credentials stored to db

--- easybuy/src/firebase/firebase.js
+++ easybuy/src/firebase/firebase.js
@@ -16,14 +16,27 @@ const config = {
   export const createUserProfileDocument = async (userAuth, additionalData) => {
       if(!userAuth) return;
     
-      const userRef = firestore.doc('users/128fdashadu');
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
 
       const snapShot = await userRef.get();
 
-      console.log(snapShot);
-      
+      if(!snapShot.exists){
+          const {displayName, email} = userAuth;
+          const createdAt = new Date();
 
-      console.log(firestore.doc('users/128fdashadu'));  
+          try{
+                await userRef.set({
+                    displayName, 
+                    email,
+                    createdAt,
+                    ...additionalData
+                })
+          } catch(error){
+                console.log('Error creating user', error.message);
+                
+          }
+      }
+      return userRef;
   };
 
 
