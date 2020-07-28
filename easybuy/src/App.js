9cf097332b8ed39c895ea4a6ff3ace10bf1c5900
neeerajtk <neerajtk560@gmail.com>
2020-07-28 22:23:23 +0530

    state

--- easybuy/src/App.js
+++ easybuy/src/App.js
@@ -30,10 +30,13 @@ class App extends Component {
               id: snapShot.id,
               ...snapShot.data()
             }
+          },()=>{
+            console.log(this.state);
           })          
         });
-        console.log(this.state);
         
+      }else{
+        this.setState({currentUser:userAuth});
       }
     });
   }
