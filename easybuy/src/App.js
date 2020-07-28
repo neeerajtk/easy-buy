d56deb491d2ef30657f90b138809619675c6a63d
neeerajtk <neerajtk560@gmail.com>
2020-07-28 22:17:48 +0530

    snpa addd

--- easybuy/src/App.js
+++ easybuy/src/App.js
@@ -20,9 +20,15 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-      
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          console.log(snapShot.data);
+          
+        })
+      }
     });
   }
 
