514904ac41e45a3bd8bb0bd207ed58b5eee45c9a
neeerajtk <neerajtk560@gmail.com>
2020-07-31 23:25:13 +0530

    dispatch added

--- easybuy/src/App.js
+++ easybuy/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import './App.css';
 
@@ -8,35 +9,28 @@ import ShopPage from './pages/shop/shop';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import Header from './components/header/header';
 import {auth,createUserProfileDocument} from './firebase/firebase';
+import {setCurrentUser} from './redux/user/user.actions';
 
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      currentUser : null
-    };
-  }
+
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser ({
               id: snapShot.id,
               ...snapShot.data()
-            }
+            })
           });         
-          console.log(this.state);
-           
-        });
-        
+                   
       }else{
-        this.setState({currentUser:userAuth});
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -59,4 +53,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps)(App);
