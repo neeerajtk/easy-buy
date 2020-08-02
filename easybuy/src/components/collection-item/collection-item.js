bc98738d36064f698a844ed1875df4afb36b0279
neeerajtk <neerajtk560@gmail.com>
2020-08-02 21:41:46 +0530

    add to cart buttons added

--- easybuy/src/components/collection-item/collection-item.js
+++ easybuy/src/components/collection-item/collection-item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CustomButton from '../custom-button/custom-button';
 import './collection-item.scss';
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
@@ -14,6 +14,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton inverted>Add to Cart</CustomButton>
 
     </div>
 )
