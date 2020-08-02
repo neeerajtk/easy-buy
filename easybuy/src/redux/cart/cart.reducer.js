a63ec04bd013ee3999378c780e3d352b337e622b
neeerajtk <neerajtk560@gmail.com>
2020-08-02 20:11:56 +0530

    action added for cart

--- easybuy/src/redux/cart/cart.reducer.js
+++ easybuy/src/redux/cart/cart.reducer.js
@@ -1,10 +1,12 @@
+import CartActionTypes from './cart.actions';
+
 const INITIAL_STATE = {
     hidden: true
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case TOGGLE_CART_HIDDEN: 
+        case CartActionTypes.TOGGLE_CART_HIDDEN: 
         return {
             ...state,
             hidden: !state.hidden
@@ -12,3 +14,5 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         default: return state;
     }
 }
+
+export default cartReducer;
