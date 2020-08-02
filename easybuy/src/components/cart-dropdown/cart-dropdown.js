52c59a02063da07817f865dabd3c16ec05458824
neeerajtk <neerajtk560@gmail.com>
2020-08-02 15:14:01 +0530

    dropdown component added

---
+++ easybuy/src/components/cart-dropdown/cart-dropdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button';
+
+import './cart-dropdown.scss';
+
+const CartDropdown = () => (
+    <div className="cart-dropdown">
+        <div className="cart-items">
+            <CustomButton>Go to Checkout</CustomButton>
+        </div>
+    </div>
+)
+
+export default CartDropdown;
