0ba3cccf808f30472b1948bf5629ea444b85280f
neeerajtk <neerajtk560@gmail.com>
2020-08-03 23:09:47 +0530

    cart item total number items disp

--- easybuy/src/components/cart-icon/cart-icon.js
+++ easybuy/src/components/cart-icon/cart-icon.js
@@ -6,10 +6,10 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
-        <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+       <ShoppingIcon className="shopping-icon"/>
+        <span className="item-count">{itemCount}</span>
     </div>
     );
 
@@ -17,7 +17,13 @@ const CartIcon = ({toggleCartHidden}) => (
         toggleCartHidden: () => dispatch(toggleCartHidden())
     });
 
+    const mapStateToProps = ({cart: {cartItems}}) => ({
+        itemCount: cartItems.reduce((accumalayedQuantity,cartItem)=>accumalayedQuantity + cartItem.quantity,
+        0
+        )
+    })
+
 export default connect(
-    null,
+    mapStateToProps,
      mapDispatchToProps
      )(CartIcon);
