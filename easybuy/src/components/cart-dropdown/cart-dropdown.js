ecfca61f0cc4feac286fc4f46e2876ea532943ed
neeerajtk <neerajtk560@gmail.com>
2020-08-07 11:43:00 +0530

    cart empty disp on empty array added

--- easybuy/src/components/cart-dropdown/cart-dropdown.js
+++ easybuy/src/components/cart-dropdown/cart-dropdown.js
@@ -7,15 +7,19 @@ import CartItem from '../cart-item/cart-item';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.scss';
+import cartIcon from '../cart-icon/cart-icon';
 
 const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
+                cartItems.length ? 
                 cartItems.map(cartItem => (
-                <CartItem key={cartItem.id} item={cartItem}
-                />
-                ))}
+                <CartItem key={cartItem.id} item={cartItem}/>
+                ))
+                :
+                <span className="empty-message">Your cart is empty !  </span>
+                }
         </div>
             <CustomButton>Go to Checkout</CustomButton>
     </div>
