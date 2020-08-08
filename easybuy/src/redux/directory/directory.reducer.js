2564f7ba807d544bf22f8d4f6440aa1cad97e38a
neeerajtk <neerajtk560@gmail.com>
2020-08-08 23:07:49 +0530

    reducr update

--- easybuy/src/redux/directory/directory.reducer.js
+++ easybuy/src/redux/directory/directory.reducer.js
@@ -32,4 +32,13 @@ const INITIAL_STATE = {
           id: 5,
           linkUrl: 'shop/mens'
         }]
-}
+};
+
+const directoryReducer = (state = INITIAL_STATE , action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+export default directoryReducer;
