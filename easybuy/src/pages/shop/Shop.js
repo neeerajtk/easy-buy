9b9f1a67181556f08493da38b70a5039311b0775
neeerajtk <neerajtk560@gmail.com>
2020-08-09 00:28:17 +0530

    preview collection added

--- easybuy/src/pages/shop/Shop.js
+++ easybuy/src/pages/shop/Shop.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-import {selectCollections} from '../../redux/shop/shop.selector';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
 
 const ShopPage = ({collections}) => (
             <div className="shop-page">
-              {
-                collections.map(({id, ...otherCollectionProps}) => (
-                  <CollectionPreview key={id} {...otherCollectionProps}/> 
-                ))
-              }
+              <CollectionsOverview/>
             </div>
         );
 
-      const mapStateToProps = createStructuredSelector({
-        collections: selectCollections
-      });
+    
  
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
