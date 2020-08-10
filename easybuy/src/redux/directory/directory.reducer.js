d48b1b607a1c515e93eedab2276fc5e70d20a437
neeerajtk <neerajtk560@gmail.com>
2020-08-10 13:24:48 +0530

    directory updated

--- easybuy/src/redux/directory/directory.reducer.js
+++ easybuy/src/redux/directory/directory.reducer.js
@@ -4,19 +4,19 @@ const INITIAL_STATE = {
           title: 'Home & Living',
           imageUrl: 'https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
           id: 1,
-          linkUrl: 'shop/home'
+          linkUrl: 'shop/hats'
         },
         {
           title: 'Electronics',
           imageUrl: 'https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
           id: 2,
-          linkUrl: 'shop/electronics'
+          linkUrl: 'shop/jackets'
         },
         {
           title: 'Automotive',
           imageUrl: 'https://images.unsplash.com/photo-1539799139339-50c5fe1e2b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80.png',
           id: 3,
-          linkUrl: 'shop/automotive'
+          linkUrl: 'shop/sneakers'
         },
         {
           title: 'womens Wear',
